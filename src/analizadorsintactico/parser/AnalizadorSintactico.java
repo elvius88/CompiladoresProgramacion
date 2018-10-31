11a1d4981bd4d026ad3861fe992dcd2edce60780
Elvio Teófilo Contreras Martínez <elviocontreras02@gmail.com>
2018-10-31 18:59:20 -0300

    Desarrollo del Analizador Sintactico

---
+++ src/analizadorsintactico/parser/AnalizadorSintactico.java
@@ -0,0 +1,130 @@
+package analizadorsintactico.parser;
+
+import analizadorlexico.entidad.Token;
+import analizadorlexico.enums.TokenEnum;
+
+/**
+ *
+ * @author elvioc
+ */
+public class AnalizadorSintactico {
+    
+    private Token token;
+    
+    public void json(){
+        element();
+    }
+    
+    public void element(){
+        switch(token.getPunteroEntrada().getLexema()){
+            case "[":
+                array();
+            case "{":
+                object();
+        }
+    }
+    public void array(){
+        if ('[' == token.getComponenteLexico()) {
+            match("[");
+            arrayPrima();
+        } else {
+            error();
+        }
+    }
+    public void arrayPrima(){
+        if (null == token.getPunteroEntrada().getLexema()) {
+            error();
+        } else switch (token.getPunteroEntrada().getLexema()) {
+            case "[":
+            case "{":
+                elementList();
+                break;
+            case "]":
+                match("]");
+                break;
+            default:
+                error();
+                break;
+        }
+    }
+    public void object(){
+        if ('{' == token.getComponenteLexico()) {
+            match("{");
+            objectPrima();
+        } else {
+            error();
+        }
+    }
+    public void objectPrima(){
+        attributeList();
+        match("}");
+    }
+    public void elementList(){
+        element();
+        elementListPrima();
+    }
+    public void elementListPrima(){
+        if (',' == token.getComponenteLexico()) {
+            match(",");
+            element();
+            elementListPrima();
+        }
+    }
+    public void attributeList(){
+        attribute();
+        attributeListPrima();
+    }
+    public void attributeListPrima(){
+        if (',' == token.getComponenteLexico()) {
+            match(",");
+            attribute();
+            attributeListPrima();
+        }
+    }
+    public void attribute(){
+        attributeName();
+        dosPuntosLexema();
+        attributeValue();
+    }
+    public void attributeName(){
+        if (token.getPunteroEntrada().getLexema().matches("*")) {
+            match(token.getPunteroEntrada().getLexema());
+        }else{
+            error();
+        }
+    }
+    public void attributeValue(){
+        if (TokenEnum.PR_BOOLEANO_FALSE.getId() == token.getComponenteLexico()) {
+            match(token.getPunteroEntrada().getLexema());
+        } else if (TokenEnum.PR_BOOLEANO_TRUE.getId() == token.getComponenteLexico()) {
+            match(token.getPunteroEntrada().getLexema());
+        } else if (TokenEnum.PR_NULL.getId() == token.getComponenteLexico()) {
+            match(token.getPunteroEntrada().getLexema());
+        } else if (TokenEnum.NUM.getId() == token.getComponenteLexico()) {
+            match(token.getPunteroEntrada().getLexema());
+        } else if (TokenEnum.STRING.getId() == token.getComponenteLexico()) {
+            match(token.getPunteroEntrada().getLexema());
+        } else if ('{' == token.getComponenteLexico() || '[' == token.getComponenteLexico()) {
+            element();
+        } else {
+            error();
+        }
+    }
+    public void dosPuntosLexema(){
+        if (':' == token.getComponenteLexico()) {
+            match(":");
+        }else {
+            error();
+        }
+    }
+    public void match(String expToken){
+        if (expToken.equals(token.getPunteroEntrada().getLexema())) {
+            getToken();
+        }else{
+            error();
+        }
+    }
+    public void getToken(){}
+    public void error(){}
+    
+}
