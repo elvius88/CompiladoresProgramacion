85f2b22d005675ccaf10988610e69ca7c1713e9f
Elvio Teófilo Contreras Martínez <elviocontreras02@gmail.com>
2018-11-02 15:50:22 -0300

    Modificaciones en el Analizador léxico Add gramatica.txt Modificación del README

--- src/analizadorlexico/main/Main.java
+++ src/analizadorlexico/main/Main.java
@@ -1,7 +1,10 @@
 package analizadorlexico.main;
 
 import analizadorlexico.entidad.AnalizadorLexico;
+import analizadorlexico.entidad.Entrada;
+import analizadorlexico.entidad.Token;
 import analizadorlexico.tabla.TablaSimbolo;
+import java.util.Map;
 
 /**
  *
@@ -20,11 +23,21 @@ public class Main {
 	//Inicializa la tabla de símbolos
         tablaSimbolo.initTablaSimbolos();
         //Inicializa el análisis sintáctico
-        AnalizadorLexico lexer = new AnalizadorLexico();
+        AnalizadorLexico lexer = new AnalizadorLexico(tablaSimbolo);
         //Se le pasa el fuente a leer
         lexer.setFuente("fuente.txt");
         //Se inicia la lectura del fuente
         lexer.leerFuente();
+        for (Token token : lexer.getArrayTokens()) {
+            System.out.println(token);
+        }
+        for (Map.Entry<String, Entrada> en : tablaSimbolo.getTabla().entrySet()) {
+            String key = en.getKey();
+            Entrada value = en.getValue();
+            System.out.println("KEY: " + key + ", VALUE: " + value);
+            
+        }
+        if (!lexer.isError()) System.out.println("\033[32mAnálisis léxico sin errores.\033[30m");
         //Termina de ejecutarse el programa
         System.exit(0);
     }
